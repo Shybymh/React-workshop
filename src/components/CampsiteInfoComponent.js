19a841c8b2903f59eea3fc98c2eac5cc5e1d5c2d
Shyby Mallanveliyil <shybyhamza@gmail.com>
2022-03-23 23:08:10 -0500

    Fetch from Server

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -3,6 +3,8 @@ import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem, Button, 
 import { Link } from 'react-router-dom';
 import { LocalForm, Control, Errors } from 'react-redux-form';
 import { Loading } from "./LoadingComponent";
+import { baseUrl } from '../shared/baseUrl';
+
 
 // Checking whether the length of input value is within the limits (2 and 15).
 // and return an error if it isn't.
@@ -92,7 +94,7 @@ function RenderCampsite({campsite}) {
     return(
         <div className="col-md-5 m-1">
             <Card>
-                <CardImg top src={campsite.image} alt={campsite.name} />
+                <CardImg top src={baseUrl + campsite.image} alt={campsite.name} />
                 <CardBody>
                     <CardText>{campsite.description}</CardText>
                 </CardBody>
